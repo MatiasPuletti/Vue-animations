112a07d54ee11d569ab8995ab40a0094bafcb382
matiaspuletti <pulettimatias@gmail.com>
2021-08-31 20:27:49 +0200

    Build Javascript transitions

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@
       @after-enter="afterEnter"
       @leave="leave"
       @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled"
     >
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
@@ -40,33 +42,63 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
-      usersAreVisible: false
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null
     };
   },
   methods: {
+    enterCancelled(el) {
+      console.log(el);
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled(el) {
+      console.log(el);
+      clearInterval(this.leaveInterval);
+    },
     afterLeave(el) {
       console.log('After leave');
       console.log(el);
     },
-    leave(el) {
+    leave(el, done) {
       console.log('Leave');
       console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
     },
     afterEnter(el) {
       console.log('After enter');
       console.log(el);
     },
-    enter(el) {
+    enter(el, done) {
       console.log('Enter');
       console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
     },
     beforeLeave(el) {
       console.log('Before leave');
       console.log(el);
+      el.style.opacity = 1;
     },
     beforeEnter(el) {
       console.log('Before enter');
       console.log(el);
+      el.style.opactity = 0;
     },
     showUsers() {
       this.usersAreVisible = true;
@@ -137,34 +169,6 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.para-enter-from {
-  /* opacity: 0;
-  transform: translateY(-30px); */
-}
-
-.para-enter-active {
-  animation: slide-scale 0.3s ease-out;
-}
-
-.para-enter-to {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
-
-.para-leave-from {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
-
-.para-leave-active {
-  /* transition: all 0.3s ease-in; */
-  animation: slide-scale 0.3s ease-out;
-}
-
-.para-leave-to {
-  /* opacity: 0;
-  transform: translateY(30px); */
-}
 .fade-button-enter-from,
 .fade-button-leave-to {
   opacity: 0;
